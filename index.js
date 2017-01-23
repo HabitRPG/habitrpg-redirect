c6eb233efdea4e9eec573819fdb076cc8eb2dbde
Sabe Jones <sabrecat@gmail.com>
2017-01-23 21:56:02 +0000

    fix(heroku): get process port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.get('*', function(req, res) {
   res.redirect('http://habitica.com' + req.url);
 })
 
-app.listen(80, function() {
-  console.log('Listening on port 80');  
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);  
 });
